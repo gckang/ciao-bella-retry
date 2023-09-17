6780509557e7012e52460be8b1cb478b79868577
gckang <gkang1219@gmail.com>
2023-09-17 04:49:23 -0400

    finished copying in?

---
+++ Pages/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ciao_bella.Pages
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
